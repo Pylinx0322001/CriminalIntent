1436ec4f6813a47212d950464d0cea20f6a53da4
Lee <171631592@qq.com>
2016-10-24 18:07:10 +0800

    chapter8

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,12 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         mId=UUID.randomUUID();
+        mDate=new Date();
     }
 
     public String getTitle() {
@@ -24,5 +28,17 @@ public class Crime {
 
     public UUID getId() {
         return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
