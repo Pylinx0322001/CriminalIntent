a9a12aa7cdff4a49dcaa94a06e9231d344f57059
Lee <171631592@qq.com>
2016-10-26 16:48:54 +0800

    photo

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -20,7 +22,10 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import java.io.File;
 import java.util.Date;
 import java.util.UUID;
 
@@ -34,13 +39,17 @@ public class CrimeFragment extends Fragment{
 
     private static final int REQUEST_DATE=0;
     private static final int REQUEST_CONTACT=1;
+    private static final int REQUEST_PHOTO=2;
 
     private Crime mCrime;
+    private File mPhotoFile;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
     private Button mReportButton;
     private Button mSuspectButton;
+    private ImageView mPhotoView;
+    private ImageButton mPhotoButton;
 
     public static CrimeFragment newInstance(UUID crimeId){
         Bundle args=new Bundle();
@@ -59,6 +68,7 @@ public class CrimeFragment extends Fragment{
 //                .getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
         UUID crimeId=(UUID)getArguments().getSerializable(ARG_CRIME_ID);
         mCrime=CrimeLab.get(getActivity()).getCrime(crimeId);
+        mPhotoFile=CrimeLab.get(getActivity()).getPhotoFile(mCrime);
     }
 
     @Override
@@ -155,6 +165,27 @@ public class CrimeFragment extends Fragment{
                 PackageManager.MATCH_DEFAULT_ONLY)==null){
             mSuspectButton.setEnabled(false);
         }
+
+        mPhotoView=(ImageView)v.findViewById(R.id.crime_photo);
+        mPhotoButton=(ImageButton)v.findViewById(R.id.crime_camera);
+        final Intent captureImage=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        boolean canTakePhoto=mPhotoFile !=null &&
+                captureImage.resolveActivity(packageManager) != null;
+        mPhotoButton.setEnabled(canTakePhoto);
+
+        if(canTakePhoto){
+            Uri uri=Uri.fromFile(mPhotoFile);
+            captureImage.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        }
+
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(captureImage,REQUEST_PHOTO);
+            }
+        });
+        updatePhotoView();
+
         return v;
     }
 
@@ -192,6 +223,8 @@ public class CrimeFragment extends Fragment{
             }finally{
                 c.close();
             }
+        }else if(requestCode==REQUEST_PHOTO){
+            updatePhotoView();
         }
     }
 
@@ -221,5 +254,15 @@ public class CrimeFragment extends Fragment{
                 mCrime.getTitle(),dateString,solvedString,suspect);
 
         return report;
+    }
+
+    private void updatePhotoView(){
+        if(mPhotoFile==null || !mPhotoFile.exists()){
+            mPhotoView.setImageDrawable(null);
+        }else{
+            Bitmap bitmap=PictureUtils.getScaledBitmap(
+                    mPhotoFile.getPath(),getActivity());
+            mPhotoView.setImageBitmap(bitmap);
+        }
     }
 }
